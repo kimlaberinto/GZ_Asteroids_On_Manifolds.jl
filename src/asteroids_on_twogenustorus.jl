af1549b40de77ea63006fdf21a84fb3d4ead8058
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-07-31 16:55:14 -0500

    Add arena

--- src/asteroids_on_twogenustorus.jl
+++ src/asteroids_on_twogenustorus.jl
@@ -1,11 +1,25 @@
 using Colors
 
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 900
+HEIGHT = 900
 BACKGROUND = colorant"grey26"
 
-function draw(g::Game)
 
+struct ArenaRect
+    geometry::Rect
+    arena_color
+end
+
+arena_left = ArenaRect(Rect(50, 50, 400, 800), colorant"black")
+arena_right =  ArenaRect(Rect(450, 450, 400, 400), colorant"black")
+
+arenas = ArenaRect[arena_left, arena_right]
+
+function draw(g::Game)
+    clear()
+    for arena in arenas
+        draw(arena.geometry, arena.arena_color, fill=true)
+    end
 end
 
 
