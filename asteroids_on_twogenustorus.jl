1a363aab2603cbac6f9951015efaac8ce38d5141
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-08-01 12:50:42 -0500

    Add asteroids to game

--- asteroids_on_twogenustorus.jl
+++ asteroids_on_twogenustorus.jl
@@ -20,6 +20,42 @@ function accelerate!(player::Player, acceleration, dt)
     return nothing
 end
 
+# Define Asteroids
+mutable struct Asteroid
+    actor::Actor
+    velocity::Array
+    angular_velocity::Float64
+end
+
+draw(asteroid::Asteroid) = draw(asteroid.actor)
+function accelerate!(asteroid::Asteroid, acceleration, dt)
+    asteroid.velocity += acceleration.*dt
+    velocity_magnitude = norm(asteroid.velocity)
+    capped_velocity_magnitude = min(velocity_magnitude, 300)
+    asteroid.velocity = normalize(asteroid.velocity) * capped_velocity_magnitude
+    return nothing
+end
+
+function init_random_asteroid()
+    size = rand(["small"])
+    image_index = rand(1:5)
+
+    image_file = "$(size)_asteroids_$(image_index).png"
+
+    random_angle = 2*pi*rand()
+    random_speed = 25 + 100*rand()
+    random_velocity = random_speed * [cos(random_angle), sin(random_angle)]
+
+    random_angular_velocity = 100 + 100*rand()
+
+    random_position = (50 + 800*rand(), 50 + 800*rand())
+
+    new_asteroid = Asteroid(Actor(image_file), random_velocity, random_angular_velocity)
+    set_position!(new_asteroid, random_position[1], random_position[2])
+
+    return new_asteroid
+end
+
 # Generic functions
 set_position!(obj, x, y) = (obj.actor.pos = (x, y))
 
@@ -48,14 +84,19 @@ end
 update_angle!(obj, dt) = obj.actor.angle += obj.angular_velocity * dt
 
 
-
 # Instantiate a global player ship
 player = Player(Actor("player.png"), [0, 0], 0)
 set_position!(player, 225, 225)
 
+# Instantiate asteroids
+asteroids = Asteroid[init_random_asteroid() for _ in 1:5]
 
 function draw(g::Game)
     clear()
+
+    for asteroid in asteroids
+        draw(asteroid)
+    end
 
     draw(player)
 
@@ -78,5 +119,11 @@ function update(g::Game, dt)
     # Player position and angle update
     update_position!(player, dt)
     update_angle!(player, dt)
+
+    # Update asteroids
+    for asteroid in asteroids
+        update_position!(asteroid, dt)
+        update_angle!(asteroid, dt)
+    end
 
 end
