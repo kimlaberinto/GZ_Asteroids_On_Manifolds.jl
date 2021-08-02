dd7527b1afffa6c46afdab99534c952694b5a541
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-08-01 21:01:57 -0500

    Reduce max speed to 500

--- asteroids_on_twogenustorus.jl
+++ asteroids_on_twogenustorus.jl
@@ -15,7 +15,7 @@ draw(player::Player) = draw(player.actor)
 function accelerate!(player::Player, acceleration, dt)
     player.velocity += acceleration.*dt
     velocity_magnitude = norm(player.velocity)
-    capped_velocity_magnitude = min(velocity_magnitude, 600)
+    capped_velocity_magnitude = min(velocity_magnitude, 500)
     player.velocity = normalize(player.velocity) * capped_velocity_magnitude
     return nothing
 end
