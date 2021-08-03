935be14f0b42945e6c5fec8ee9952a3474e2b5d8
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-08-02 20:22:26 -0500

    Add bullets

--- asteroids_on_twogenustorus.jl
+++ asteroids_on_twogenustorus.jl
@@ -56,6 +56,35 @@ function init_random_asteroid()
     return new_asteroid
 end
 
+# Define Bullet
+mutable struct Bullet
+    actor::Circle
+    velocity::Array
+end
+
+draw(bullet::Bullet) = draw(bullet.actor, colorant"white")
+
+function update_position!(obj::Bullet, dt)
+    x, y = obj.actor.x, obj.actor.y
+
+    v = obj.velocity
+    displacement = v * dt
+
+    dx = displacement[1]
+    dy = displacement[2]
+
+    # Apply velocity
+    new_x, new_y = (x + dx, y + dy)
+
+    # Wrap around if necessary
+    new_x, new_y = two_genus_wrap_position(new_x, new_y)
+
+    # Update
+    set_position!(obj, new_x, new_y)
+    return nothing
+end
+set_position!(bullet::Bullet, x, y) = ((bullet.actor.x, bullet.actor.y) = (x, y))
+
 # Generic functions
 set_position!(obj, x, y) = (obj.actor.pos = (x, y))
 
@@ -84,13 +113,16 @@ end
 update_angle!(obj, dt) = obj.actor.angle += obj.angular_velocity * dt
 
 function reset()
-    global player, asteroids
+    global player, asteroids, bullets
     # Instantiate a global player ship
     player = Player(Actor("player.png"), [0, 0], 0)
     set_position!(player, 225, 225)
 
     # Instantiate asteroids
     asteroids = Asteroid[init_random_asteroid() for _ in 1:5]
+
+    # clear bullet array
+    bullets = Bullet[Bullet(Circle(225, 225, 10), [0, 0])]
 end
 
 reset()
@@ -100,6 +132,10 @@ function draw(g::Game)
 
     for asteroid in asteroids
         draw(asteroid)
+    end
+
+    for bullet in bullets
+        draw(bullet)
     end
 
     draw(player)
@@ -128,6 +164,21 @@ function update(g::Game, dt)
     for asteroid in asteroids
         update_position!(asteroid, dt)
         update_angle!(asteroid, dt)
+    end
+
+    if g.keyboard.SPACE
+        player_direction = [cos(angle_rad), sin(angle_rad)]
+        bullet_speed = 100 * player_direction
+        new_bullet = Bullet(Circle(player.actor.pos[1], player.actor.pos[2], 5), bullet_speed)
+        set_position!(new_bullet, player.actor.pos[1], player.actor.pos[2])
+        push!(bullets, new_bullet)
+    end
+
+    #update bullets
+    if length(bullets) > 0
+        for bullet in bullets
+            update_position!(bullet, dt)
+        end
     end
 
     for asteroid in asteroids
